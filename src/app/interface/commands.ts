9a8673641f5f784831f892e0f8ab79f88ef8ef0d
MorenoLucas <sl.lucasmoreno@gmail.com>
2021-05-04 10:24:24 +0200

    interafaces

--- src/app/interface/commands.ts
+++ src/app/interface/commands.ts
@@ -1,8 +1,11 @@
-import { Coordinates } from './square';
-
 export interface Commands {
   direction: 'L' | 'R' | 'A';
   orientation: 'N' | 'E' | 'S' | 'W';
   coordinates: Coordinates;
   successTrip: boolean;
+}
+
+export interface Coordinates {
+  xWidth: number;
+  yHeight: number;
 }
--- src/app/interface/square.ts
+++ src/app/interface/square.ts
@@ -2,8 +2,3 @@ export interface Square {
   width: number;
   height: number;
 }
-
-export interface Coordinates {
-  xWidth: number;
-  yHeight: number;
-}
